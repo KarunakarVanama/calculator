081d1dbedad7f1930c56cd4b88b524663ee11952
KarunakarVanama <karunakar.vanama1729@gmail.com>
2019-09-25 13:56:35 +0530

    added calculator

---
+++ calculator/Controllers/SubtractController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace calculator.Controllers
+{
+    public class SubtractController : ApiController
+    {
+        // GET: api/Subtract
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "vanama", "Karunakar" };
+        }
+
+        // GET: api/Subtract/sub
+        public int Get(int a, int b)
+        {
+            return a-b;
+        }
+
+        
+    }
+}
---
+++ calculator/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace calculator.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/add
+        public int Get(int a, int b)
+        {
+            return a+b;
+        }
+
+    }
+}
